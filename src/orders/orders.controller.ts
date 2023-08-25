3034e392a9fa5384528bdfc89bbdae2ce3a6be5e
Javier <javiercastillopena@gmail.com>
2023-08-24 20:11:11 -0500

    feat: Add route protection to order controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -2,12 +2,19 @@ import {
   Controller,
   Get,
   Req,
+  UseGuards,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { Order } from './order.entity';
 import { Request } from 'express';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
+import { Role } from '../enums/role.enum';
 
 @Controller('orders')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(Role.Admin)
 export class OrdersController {
   constructor (private readonly ordersService: OrdersService) {}
 
